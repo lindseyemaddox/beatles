420d1b9a5afc06f9d67ec37f1c5474a07eb34b66
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-21 16:34:01 -0500

    one so far

---
+++ main.js
@@ -0,0 +1,42 @@
+
+// Output the following value to the console.
+
+// Paul McCartney was in the Beatles from 1960 to 1970. He was born in 1942. He contributed heavily to the Magical Myster Tour Album.
+
+
+const beatles = {
+  albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+  history: {
+      formed: 1960,
+      disbanded: 1970
+  },
+  members: [
+      {
+          name: "George Harrison",
+          birth: 1943,
+          death: 2001
+      },
+      {
+          name: "Paul McCartney",
+          birth: 1942,
+          death: null
+      },
+      {
+          name: "John Lennon",
+          birth: 1940,
+          death: 1980
+      },
+      {
+          name: "Ringo Starr",
+          birth: 1940,
+          death: null
+      }
+  ]
+};
+
+
+
+for (var propertyKey in beatles) {
+  console.log(beatles.members[1].name + " poop")
+
+};
